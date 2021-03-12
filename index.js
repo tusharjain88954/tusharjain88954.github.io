dfb26f92434bc82b5a19e4657de420e75a59eb33
Tushar Jain <54166594+tusharjain88954@users.noreply.github.com>
2021-03-12 22:51:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("working!");
+
+let btn = document.querySelector(".btn.btn-outline-success.me-2");
+
+btn.addEventListener("click", function (e) {
+  location.href = "mailto:tusharjain11282001@gmail.com";
+});
